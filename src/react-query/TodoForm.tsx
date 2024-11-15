81071944dcfce9f320ea181638307edb3dcab084
vasilkotsev <vasil.kotsev@gmail.com>
2024-11-15 18:14:16 +0200

    Implement handling mutation errors and mutation progress

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -6,10 +6,10 @@ import { Todo } from "./hooks/useTodos";
 const TodoForm = () => {
   const queryClient = useQueryClient(); //хук, достъпващ нашия queryClient, дефиниран в main.tsx]
 
-  const addTodo = useMutation({
+  const { mutate, error, isLoading } = useMutation<Todo, Error, Todo>({
     mutationFn: (todo: Todo) =>
       axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .post("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
     onSuccess: (savedTodo, newTodo) => {
       /* Approach 1: Invalidating the cache=> казваме на RQuery, че каквото имаме в кеша 
@@ -23,34 +23,40 @@ const TodoForm = () => {
         savedTodo,
         ...todos,
       ]);
+
+      if (ref.current) ref.current.value = "";
     },
   });
 
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form
-      className="row mb-3"
-      onSubmit={(e) => {
-        e.preventDefault();
+    <>
+      {error && <div className="alert alert-danger">{error.message}</div>}
+      <form
+        className="row mb-3"
+        onSubmit={(e) => {
+          e.preventDefault();
 
-        if (ref.current?.value) {
-          addTodo.mutate({
-            id: 0,
-            title: ref.current?.value,
-            completed: false,
-            userId: 1,
-          });
-        }
-      }}
-    >
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+          if (ref.current?.value)
+            mutate({
+              id: 0,
+              title: ref.current?.value,
+              completed: false,
+              userId: 1,
+            });
+        }}
+      >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button disabled={isLoading} className="btn btn-primary">
+            {isLoading ? "Adding..." : "Add"}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
